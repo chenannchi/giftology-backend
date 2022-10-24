377bb938e106932acad5535b871d200bbb2fd189
creyes25 <carolina.reyes2022@gmail.com>
2022-10-24 15:15:49 -0500

    create wishlist model

---
+++ controllers/wishlists.js
@@ -0,0 +1,1 @@
+import { Wishlist } from "../models/wishlist.js"
---
+++ models/wishlist.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema
+
+const wishlistSchema = new Schema({
+  name: String,
+  type: String,
+  description: String,
+  items: [{ type: Schema.Types.ObjectId, ref: 'Item'}]
+}, {
+  timestamps: true
+})
+
+const Wishlist = mongoose.model('Wishlist', wishlistSchema)
+
+export { Wishlist }
