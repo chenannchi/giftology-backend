f9a69a0f3f48d6cd214325e683e99dc16d3f8332
creyes25 <carolina.reyes2022@gmail.com>
2022-10-24 18:30:42 -0500

    add update functionality to wishlist

--- controllers/wishlists.js
+++ controllers/wishlists.js
@@ -37,9 +37,25 @@ const show = async (req, res) => {
   }
 }
 
+const update = async (req, res) => {
+  try {
+    const wishlist = await Wishlist.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    )
+    res.status(200).json(wishlist)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(err)
+  }
+}
+
+
 
 export {
   create,
   index,
   show,
+  update
 }
--- routes/wishlists.js
+++ routes/wishlists.js
@@ -12,5 +12,6 @@ router.use(decodeUserFromToken)
 router.get('/', checkAuth, wishlistsCtrl.index)
 router.get('/:id', checkAuth, wishlistsCtrl.show)
 router.post('/', checkAuth, wishlistsCtrl.create)
+router.put('/:id', checkAuth, wishlistsCtrl.update)
 
 export { router }
