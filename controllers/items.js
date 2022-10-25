30f86fd0b3f289544c0f811a2923ebd122d9fcbf
creyes25 <carolina.reyes2022@gmail.com>
2022-10-24 21:41:00 -0500

    add functionality to add an item

--- controllers/items.js
+++ controllers/items.js
@@ -1,2 +1,17 @@
-import { Profile } from "../models/profile.js"
+import { Wishlist } from "../models/wishlist.js"
 import { Item } from "../models/item.js"
+
+const create = async (req, res) => {
+  try {
+    const item = await Item.create(req.body)
+    res.status(201).json(item)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(err)
+  }
+}
+
+
+export {
+  create
+}
--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(formData.parse())
 app.use('/api/profiles', profilesRouter)
 app.use('/api/auth', authRouter)
 app.use('/api/wishlists', wishlistsRouter)
-app.use('/wishlists/items', itemsRouter)
+app.use('/api/items', itemsRouter)
 
 app.use(function (req, res, next) {
   res.status(404).json({ err: 'Not found' })
