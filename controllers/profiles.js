ad60a1a5cd6e4e910c1adc0967a4b5930d4ed2e8
AWeidenkopf <oliveira.anacalado@gmail.com>
2022-10-26 11:43:40 -0400

    populate  wishlist in profile show page

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -125,8 +125,11 @@ const deleteFriend = async (req, res) => {
 const show = async (req, res) => {
   try {
     const profile = await Profile.findById(req.params.id)
+    .populate('friends')
+    .populate('wishlists')
+    res.status(200).json(profile)
   } catch (error) {
-    
+    res.status(500).json(error)
   }
 }
 
